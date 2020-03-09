9c12f9fa1757ee93a6f4b6dd3654d606e6f5b705
M4LP4 <61868109+M4LP4@users.noreply.github.com>
2020-03-09 19:47:33 +0100

    Update from laptop

--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -2,5 +2,6 @@ public class Start {
     public static void main(String[] args0){
         System.out.println("Hello Git!");
         System.out.println("Update git");
+        System.out.println("Laptop update");
     }
 }
